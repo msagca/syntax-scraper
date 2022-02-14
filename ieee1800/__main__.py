066b55384f3dfc6d1ebaf4010e694baec89d7d5f
mustafa <mustafa@localhost.localdomain>
2022-02-15 01:29:04 +0300

    implement ieee1800 scraper

---
+++ ieee1800/__main__.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import antlr4
+import argparse
+import pdfplumber
+import sys
+sys.path.append('../syntax_scraper')
+from ieeeAnnex.ieeeAnnexLexer import ieeeAnnexLexer
+from ieeeAnnex.ieeeAnnexParser import ieeeAnnexParser
+from ieeeAnnex.ieeeAnnexListenerChild import ieeeAnnexListenerChild
+
+def main():
+	arg_parser = argparse.ArgumentParser()
+	arg_parser.add_argument('input', help='IEEE Std 1800-xxxx PDF')
+	arg_parser.add_argument('-n', required=True, metavar='grammar_name', help='Grammar name')
+	arg_parser.add_argument('-s', type=int, required=True, metavar='start_page', help='Annex A start page')
+	arg_parser.add_argument('-e', type=int, required=True, metavar='end_page', help='Annex A end page')
+	args = arg_parser.parse_args()
+	annex_text = ''
+	bold_text = ''
+	temp_text = ''
+	reached_start = False
+	reached_end = False
+	with pdfplumber.open(args.input) as pdf:
+		for nn in range(args.s, args.e+1):
+			page = pdf.pages[nn]
+			for cc in page.chars:
+				if temp_text.find('Footnotes') != -1:
+					reached_end = True
+					break
+				if reached_start:
+					if cc['fontname'] == 'BHDFJL+TimesNewRomanPSMT':
+						if bold_text:
+							annex_text += "'" + bold_text + "'"
+							bold_text = ''
+						annex_text += cc['text']
+					elif cc['fontname'] == 'BVXWSQ+CourierNew,Bold':
+						if not cc['text'].isspace():
+							if bold_text in ('[', ']', '(', ')', '{', '}') or cc['text'] in ('[', ']', '(', ')', '{', '}'):
+								if bold_text:
+									annex_text += "'" + bold_text + "'"
+									bold_text = ''
+							bold_text += cc['text']
+						elif bold_text:
+							annex_text += "'" + bold_text + "'"
+							bold_text = ''
+				elif temp_text.find('Source text') != -1:
+					reached_start = True
+				if cc['fontname'] == 'BHDEOM+Arial-BoldMT':
+					temp_text += cc['text']
+			if reached_end:
+				break
+	input_stream = antlr4.InputStream(annex_text)
+	lexer_obj = ieeeAnnexLexer(input_stream)
+	token_stream = antlr4.CommonTokenStream(lexer_obj)
+	token_stream.fill()
+	parser_obj = ieeeAnnexParser(token_stream)
+	tree_obj = parser_obj.formal_syntax()
+	listener_obj = ieeeAnnexListenerChild(name=args.n)
+	walker_obj = antlr4.ParseTreeWalker();
+	walker_obj.walk(listener_obj, tree_obj)
+	fp = open(f'{args.n}.g4', 'w')
+	fp.write(listener_obj.grammar_definition)
+	fp.close()
+
+if __name__ == '__main__':
+	main()
---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+	name='syntax_scraper',
+	version='1.0.0',
+	author='Mustafa Said Ağca',
+	long_description_content_type='text/markdown',
+	url='https://github.com/msagca/syntax_scraper',
+	classifiers=[
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+	packages=['ieee1800'],
+	include_package_data=True,
+	install_requires=['antlr4-python3-runtime', 'pdfplumber'],
+	entry_points={'console_scripts': ['scrape-ieee1800=ieee1800.__main__:main']},
+)
